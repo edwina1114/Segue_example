02c6aa0de0e1034b3ab9c84db6103688a134376b
edwina1114 <ta517152139@gmail.com>
2020-11-19 11:21:26 +0800

    轉場練習

---
+++ Segue_example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Segue_example
+//
+//  Created by NDHU_CSIE on 2020/11/19.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet var sendMessage : UITextField!
+    @IBOutlet var receiveMessage : UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "change" {
+            let des = segue.destination as! SecondViewController
+            des.passMessage = sendMessage.text!
+            
+            }
+        }
+}
+
